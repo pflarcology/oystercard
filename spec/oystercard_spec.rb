186830e7d57e2d6e8b250209d4a5e4222088d74f
Chris Cooper <cjcooper252@gmail.com>
2016-11-15 11:15:36 +0000

    initializes oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
 require 'oystercard'
 
-  describe Oystercard do
+describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
+  describe '#initialization' do
+
+    it 'starts wihth a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
+
+
+end
