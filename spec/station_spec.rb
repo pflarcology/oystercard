f4b9b41c80855179aa388989972c8105a8bf67c0
pflarcology <rhrg500@gmail.com>
2016-11-17 10:53:10 +0000

    adds station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,18 @@
+require 'station'
+describe Station do
+
+  subject {described_class.new("Aldgate East", 1)}
+
+  context 'testing initialization' do
+
+    it 'should respond to calling its name' do
+      expect(subject.name).to eq("Aldgate East")
+    end
+
+    it 'should respond to zone' do
+      expect(subject.zone).to eq 1
+    end
+
+  end
+
+end
